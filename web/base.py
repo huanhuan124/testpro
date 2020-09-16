7a8cd78d5888a008e43c1bcac7df59f825efd18f
huanhuan124 <529308803@qq.com>
2020-09-17 07:05:44 +0800

    ADD TEST_SELENIUM

--- web/base.py
+++ web/base.py
@@ -1,10 +1,30 @@
+import os
+
 from selenium import webdriver
 
 
-class Base:
+class Base():
+
+    # def __init__(self):
+    #     self.driver = None
 
     def setup(self):
+
+        # 运用多浏览器时，提示找不到driver？？？？未找到原因
+        # browser = os.getenv("browser")
+        # # self.driver = None
+        #
+        # if browser == 'firefox':
+        #     self.driver = webdriver.Firefox()
+        #
+        # elif browser == 'chrome':
+        #     print("chrome^^^^^^^^^^^^^^^^^^")
+        #     self.driver == webdriver.Chrome()
+        #     print(self.driver)
+
+
         self.driver = webdriver.Chrome()
+
         self.driver.maximize_window()
         self.driver.implicitly_wait(3)
 
